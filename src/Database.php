970964da8735c694bea737e48124904a2709a495
bfrancizod <bastien.francizod@gmail.com>
2026-03-25 16:10:10 +0100

    Corrige la connexion a la base MariaDB

--- src/Database.php
+++ src/Database.php
@@ -12,14 +12,15 @@ class Database
         try {
             $pdo = new PDO(
                 "mysql:host=127.0.0.1;dbname=help_me_stage;charset=utf8mb4",
-                "helpmeuser",
-                "motdepasse123"
+                "helpmestage",
+                "aaaa"
             );
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             return $pdo;
+
         } catch (PDOException $e) {
             die("Erreur connexion DB : " . $e->getMessage());
         }
